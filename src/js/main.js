235d42c462d4b8c9c9c12690d8d59a87f0f1d931
natalia <telula@icloud.com>
2018-08-22 00:20:06 +0300

    bootstrap4_gulp

---
+++ src/js/main.js
@@ -0,0 +1,23 @@
+
+
+// Импортируем jQuery
+//= ../../bower_components/jquery/dist/jquery.js
+
+// Импортируем Popper
+//= ../../node_modules/popper.js/dist/umd/popper.js
+
+// Импортируем необходимые js-файлы Bootstrap 4
+//= ../../bower_components/bootstrap/js/dist/util.js
+//= ../../bower_components/bootstrap/js/dist/alert.js
+//= ../../bower_components/bootstrap/js/dist/button.js
+//= ../../bower_components/bootstrap/js/dist/carousel.js
+//= ../../bower_components/bootstrap/js/dist/collapse.js
+//= ../../bower_components/bootstrap/js/dist/dropdown.js
+//= ../../bower_components/bootstrap/js/dist/modal.js
+//= ../../bower_components/bootstrap/js/dist/scrollspy.js
+//= ../../bower_components/bootstrap/js/dist/tab.js
+//= ../../bower_components/bootstrap/js/dist/tooltip.js
+//= ../../bower_components/bootstrap/js/dist/popover.js
+//= ../../bower_components/bootstrap/js/dist/index.js
+
+// Импортируем другие js-файлы
